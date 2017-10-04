18b5d22042229a2280c96afc6027fc435745a8f1
Oleg Cherniavskyi <cherniavskyi.utd@gmail.com>
2017-10-04 15:38:08 +0300

    finished desktop version and add some animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+$(document).ready( function() {
+
+    //анимация переходов
+
+    new WOW().init();
+
+    //переходы (якоря)
+
+    $('a[href^="#"]').bind('click.smothscroll', function() {
+        var target = $(this).attr('href'),
+            bl_top = $(target).offset().top;
+        $('body, html').animate({scrollTop: bl_top}, 700);
+        return false;
+    });
+
+    //button--fixed
+
+    var top_show = 150;
+    var delay = 250;
+    $(document).ready(function() {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > top_show) $('.button--fixed').fadeIn(200);
+            else $('.button--fixed').fadeOut(200);
+        });
+        $('.button--fixed').click(function () {
+            $('body, html').animate({
+                scrollTop: 0
+            }, delay);
+        });
+    });
+
+});
